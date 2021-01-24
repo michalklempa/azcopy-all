c221fe0dca33a72889bfb856d90708ef9b143c3c
Michal Klempa <michal.klempa@gmail.com>
2021-01-24 21:47:40 +0100

    Update

--- update-tags.sh
+++ update-tags.sh
@@ -5,7 +5,9 @@ AZCOPY_VERSION=$(echo $AZCOPY_RELEASE | jq -r ".tag_name" | sed -s 's/^v//')
 AZCOPY_DATE=$(date -d "$(echo $AZCOPY_RELEASE | jq -r ".published_at")" +'%Y%m%d')
 AZCOPY_URL="https://azcopyvnext.azureedge.net/release${AZCOPY_DATE}/azcopy_linux_amd64_${AZCOPY_VERSION}.tar.gz"
 
-sed '1c ARG UPSTREAM_URL='"${AZCOPY_URL}" Dockerfile
+sed -i '1c ARG UPSTREAM_URL='"${AZCOPY_URL}" Dockerfile
+git config --global user.name 'Github Actions Michal'
+git config --global user.email 'michal.klempa@gmail.com'
 git add Dockerfile
 git commit -m"Upstream version bump to ${AZCOPY_VERSION}"
 git push
